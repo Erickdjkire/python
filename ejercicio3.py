426e97595ad02efe1ba5a8c03c2bae80a8df3329
Erickdjkire <torres.erick.edison@gmail.com>
2024-11-21 01:40:21 -0300

    Add files via upload
    ejercicios basicos de programacion en python

---
+++ ejercicio3.py
@@ -0,0 +1,16 @@
+# 3.- Realizar un Programa en PYTHON que tenga  un arreglo de tamaño 10 con números enteros 
+# (números positivos ó negativos). Mostrar la cantidad de números positivos y negativos que hay en dicho arreglo
+
+lista = []
+for i in range(1,11):
+    numeros = int(input("ingrese número: "))
+    lista.append(numeros)
+    mayor = 0
+    menor = 0
+    for o in lista:
+        if o > 0:
+           mayor = mayor+1
+        else:
+            menor = menor+1
+print("los números positivos son:",mayor,"y los números negativos son:",menor)
+print("los números en lista son: ",lista)
