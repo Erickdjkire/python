aa99b5793b8cd05c02aa97abb117287aa3a53f0e
Erickdjkire <torres.erick.edison@gmail.com>
2024-11-21 01:41:43 -0300

    Add files via upload

---
+++ eje002.py
@@ -0,0 +1,21 @@
+# 1.- Realizar Programa en PYTHON que maneje un arreglo de tamaño 5 
+# y que entregue la Suma de todos los elementos del arreglo que tenga .
+
+lista = []
+
+ing1 = int(input("ingrese un número: "))
+lista.append(ing1)
+ing2 = int(input("ingrese un número: "))
+lista.append(ing2)
+ing3 = int(input("ingrese un número: "))
+lista.append(ing3)
+ing4 = int(input("ingrese un número: "))
+lista.append(ing4)
+ing5 = int(input("ingrese un número: "))
+lista.append(ing5)
+
+suma = ing1+ing2+ing3+ing4+ing5
+
+print("la lista contiene", lista,"y su resultado es: ",suma)
+
+
